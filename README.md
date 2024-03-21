d16db90c2ea634663e4cf7343edfc7845ebd9d9b
Masetto96 <stefano.scola@protonmail.com>
2024-03-21 21:27:52 +0100

    implemented fix for parsing, ready to conduct some analysis

--- README.md
+++ README.md
@@ -1,41 +1,30 @@
-# makam-parser
-![HoldrianCommas](Division-of-the-whole-tone-into-nine-commas.png)
+# Turkish Makam Music XML Parser
+
+![HoldrianCommas](9commas-tone-division.png)
+
+This repository contains a parser designed specifically for parsing Turkish Makam Music (TMM) from MusicXML files. The parser is tailored to handle the [SymbTr dataset](https://github.com/MTG/SymbTr).
+
+To accommodate Holdrian commas, the parser first updates alter values present in MusicXML, according to the accidental name. In TMM, a tone is divided into 9 intervals, hence alter values range from -9 to +9.
+
+To avoid error when parsing with music21, key signatures are removed from the MusicXML files. The current implementation of music21 does not support non-western accidental types.
+
+Finally, the parser adjusts microtone values for pitches, based on the alter value. The adjustment is made in cents.
+
+## Setup
+```bash
+pip install music21
+```
 
 ## Usage
+```python
+from TMMFix.parse_makam_xml import fix_m21_parsing_makam
 
-You can parse a makam musicx-xml file by running `parse_makam_xml.py`:
+FILE_NAME = "hicaz--sarki--aksaksemai--sezdim_dargin--rifat_ayaydin.xml"
 
-#### Originally, this was the mapping from accidental name to alter value:
-
-```
-altervalues = {
-    "quarter-flat": "-0.5",
-    "slash-flat": None,
-    "flat": "-1",
-    "double-slash-flat": None,
-    "quarter-sharp": "+0.5",
-    "slash-sharp": None,
-    "sharp": "+1",
-    "slash-quarter-sharp": None,
-}
-```
-
-#### This parser will use the following mapping allowing for microtonal intervals.
-```
-altervalues = {
-    "flat": -9,
-    "sharp": +9,
-    "quarter-flat": -4,
-    "quarter-sharp": +4,
-    "slash-flat": -5,
-    "slash-quarter-sharp": +5,
-    "double-slash-flat": -8,
-    "slash-sharp": +8,
-}
-
+m21_stream_score = fix_m21_parsing_makam(FILE_NAME)
 ```
 
 ## External Links
 - https://github.com/MTG/SymbTr
 - https://github.com/burakuyar/MusicXMLConverter
-
+- https://github.com/cuthbertLab/music21
---
+++ TMMFix/parse_makam_xml.py
@@ -0,0 +1,125 @@
+from music21 import *
+import xml.etree.ElementTree as ET
+
+ALTER_VALUES = {
+    "quarter-flat": "-1",
+    "slash-flat": "-4",
+    "double-slash-flat": "-8",
+    "flat": "-5",
+    "double-flat": "-9",
+    "quarter-sharp": "+1",
+    "sharp": "+4",
+    "slash-quarter-sharp": "+5",
+    "slash-sharp": "+8",
+    "double-sharp": "+9",
+}
+
+TONE_DIVISION = 200 / 9
+
+def _describe_key_signature(tree):
+    """Print information about key signature in the score."""
+    notes = []
+    accidentals = []
+    for k in tree.iter("key"):
+        for ks in k.findall("key-step"):
+            notes.append(ks.text)
+        for ka in k.findall("key-accidental"):
+            accidentals.append(ka.text)
+
+    print("The key signature of this score has:")
+    for i in range(len(notes)):
+        print("-", notes[i], accidentals[i])
+
+
+def _update_alter_values_in_xml(notes):
+    """
+    Update the alter values in the XML representation of the notes.
+
+    Parameters:
+        notes (list): A list of notes.
+
+    Returns:
+        set: A set of accidentals found in the notes.
+    """
+    # Set to store the names of the accidentals found in the notes
+    accidentals_found = set()
+
+    # Iterate over each note in the list
+    for note in notes:
+        # Find the pitch element in the note
+        pitch = note.find(".//pitch")
+
+        # If pitch is not found, continue to the next note
+        if pitch is None:
+            continue
+
+        # Find the accidental element in the note
+        accidental = note.find("accidental")
+
+        # If accidental is not found, set accidental_text to None
+        accidental_text = accidental.text if accidental is not None else None
+
+        # If accidental_text is found in ALTER_VALUES, update the alter value
+        if accidental_text in ALTER_VALUES:
+            new_alter_value = ALTER_VALUES[accidental_text]
+
+            # Find the alter element in the pitch, if not found, create a new element
+            alter_element = pitch.find("alter")
+            if alter_element is None:
+                alter_element = ET.SubElement(pitch, "alter")
+                alter_element.tail = "\n       "
+
+            # Set the text of the alter element to the new_alter_value
+            alter_element.text = str(new_alter_value)
+            accidentals_found.add(accidental_text)
+
+    return accidentals_found
+
+
+def fix_m21_parsing_makam(file_name: str) -> stream.Stream:
+    """
+    Fix the parsing of a Makam XML file by updating the alter values of notes,
+    removing the key signature, and adding microtone deviation to pitch based on the alter value.
+
+    Parameters:
+        file_name (str): The name of the XML file to be fixed.
+
+    Returns:
+        notes (music21.stream.Stream): The stream of notes in the fixed XML file.
+    """
+    # Parse the XML file
+    try:
+        tree = ET.parse(file_name)
+    except FileNotFoundError:
+        print(f"File {file_name} not found")
+        exit(1)
+
+    _describe_key_signature(tree)
+
+    # Update the alter values of notes
+    notes = tree.findall(".//note")
+    accidentals_found = _update_alter_values_in_xml(notes)
+
+    print(f"Accidentals found and updated: {accidentals_found}")
+
+    # Remove the key signature so it can be parsed by music21
+    for att in tree.iter("attributes"):
+        if att.find("key") != None:
+            att.remove(att.find("key"))
+
+    # Save the fixed XML file with a new name
+    new_file_name = file_name.replace(".xml", "_fixed.xml")
+    tree.write(new_file_name)
+
+    # Parse the fixed XML score with music21
+    score = converter.parse(new_file_name)
+
+    # Convert the notes to a stream
+    notes = score.flatten().notes.stream()
+
+    # Assign microtone deviation in cents based on alter value
+    for n in notes:
+        if n.pitch.accidental:
+            n.pitch.microtone = n.pitch.alter * TONE_DIVISION
+
+    return notes
